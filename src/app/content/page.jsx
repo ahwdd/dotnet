8eb643e3d79d29ddbd44bc89dd7fb3da5c75d5f3
ogamal-user <ogamal@arabhardware.net>
2024-10-14 17:48:34 +0300

    [ADD] the content page initially

---
+++ src/app/content/page.jsx
@@ -0,0 +1,31 @@
+function Content() {
+  return (
+    <div className="h-screen border-2 border-red-500 relative top-0 mt-32 ">
+      {/* <h1 className="bg-red-500">content121424</h1>
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1>{" "}
+      <h1 className="bg-red-500">content</h1> */}
+    </div>
+  );
+}
+
+export default Content;
--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,153 +1,266 @@
-"use client"
-import dynamic from 'next/dynamic';
-import { useRouter } from 'next/navigation';
+"use client";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
 import AppBar from "@/components/appbar";
 import Footer from "@/components/footer";
 import SearchPage from "@/components/search_page";
 import { searchData } from "@/components/search_page/data";
 import { useState, useEffect } from "react";
-const MediaPlayer = dynamic(() => import('./media-player'));
-const SearchParamsComponent = dynamic(() => import('./searchParamsComponent'));
-const BlogsPlayer = dynamic(() => import('./blogs-player'));
-const ProductsPlayer = dynamic(() => import('./products-player'));
-import toast from 'react-hot-toast';
-import ToasterComponent from "@/components/toaster_top"
-import { findMaxTotal, getRates, processData, getfetchedData,
-  checkLocation, getTempData, getNewestShorts, getDailyNews, getTrendingData } from "./utils/functions";
+const MediaPlayer = dynamic(() => import("./media-player"));
+const SearchParamsComponent = dynamic(() => import("./searchParamsComponent"));
+const BlogsPlayer = dynamic(() => import("./blogs-player"));
+const ProductsPlayer = dynamic(() => import("./products-player"));
+import toast from "react-hot-toast";
+import ToasterComponent from "@/components/toaster_top";
+import Content from "../content/page";
+import {
+  findMaxTotal,
+  getRates,
+  processData,
+  getfetchedData,
+  checkLocation,
+  getTempData,
+  getNewestShorts,
+  getDailyNews,
+  getTrendingData,
+} from "./utils/functions";
 
 export default function Page({}) {
   const router = useRouter();
-  const [bgImg, setBgImg] = useState(searchData[0].backgroundImg)
-  const [searchValue, setSearchValue] = useState(null)
-  const [searchCachValue, setSearchCachValue] = useState(null)
-  const [initialSearchValue, setInitialSearchValue] = useState(null)
-  const [isPopupOpen, setIsPopupOpen] = useState(false)
-  const [isBlogPopupOpen, setIsBlogPopupOpen] = useState(false)
-  const [isProductPopupOpen, setIsProductPopupOpen] = useState(false)
-  const [isExpanded, setIsExpanded] = useState(false)
-  const [actionDropdownValue, setActionDropdownValue] = useState(0)
-  const [activeTabIndex, setActiveTabIndex] = useState(0)
-  const [searchTypeDropdownValue, setSearchTypeDropdownValue] = useState(0)
-  const [perPage, setPerPage] = useState(6)
+  const [bgImg, setBgImg] = useState(searchData[0].backgroundImg);
+  const [searchValue, setSearchValue] = useState(null);
+  const [searchCachValue, setSearchCachValue] = useState(null);
+  const [initialSearchValue, setInitialSearchValue] = useState(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isBlogPopupOpen, setIsBlogPopupOpen] = useState(false);
+  const [isProductPopupOpen, setIsProductPopupOpen] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [actionDropdownValue, setActionDropdownValue] = useState(0);
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const [searchTypeDropdownValue, setSearchTypeDropdownValue] = useState(0);
+  const [perPage, setPerPage] = useState(6);
   const [currentPage, setCurrentPage] = useState(1);
-  const [hasMore, setHasMore] = useState(null)
-  const [totalAvailable, setTotalAvailable] = useState(null)
-  const [suggested, setSuggested] = useState(null)
+  const [hasMore, setHasMore] = useState(null);
+  const [totalAvailable, setTotalAvailable] = useState(null);
+  const [suggested, setSuggested] = useState(null);
   const [totalPages, setTotalPages] = useState(1);
-  const [vidDis, setVidDis] = useState('full') //full / small
-  const [currentVid, setCurrentVid] = useState(null)
-  const [currentBlog, setCurrentBlog] = useState(null)
-  const [currentProduct, setCurrentProduct] = useState(null)
-  const [activeVidIndex, setActiveVidIndex] = useState(0)
-  const [currencyValue, setCurrencyValue] = useState('EGP')
-  const [isMounted, setIsMounted] = useState(false)
-  const [statusData, setStatusData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [shortsTotal, setShortsTotal] = useState(null)
+  const [vidDis, setVidDis] = useState("full"); //full / small
+  const [currentVid, setCurrentVid] = useState(null);
+  const [currentBlog, setCurrentBlog] = useState(null);
+  const [currentProduct, setCurrentProduct] = useState(null);
+  const [activeVidIndex, setActiveVidIndex] = useState(0);
+  const [currencyValue, setCurrencyValue] = useState("EGP");
+  const [isMounted, setIsMounted] = useState(false);
+  const [statusData, setStatusData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [shortsTotal, setShortsTotal] = useState(null);
 
-  const [fetchedData, setFechedData] = useState(null)
-  const [newSearchData, setNewSearchData] = useState(null)
-  const [weather, setWeather] = useState(null)
-  const [location, setLocation] = useState(null)
-  const [terndingData, setTrendingData] = useState(null)
-  const [dailyNews, setDailyNews] = useState(null)
-  const [rates, setRates] = useState(null)
+  const [fetchedData, setFechedData] = useState(null);
+  const [newSearchData, setNewSearchData] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [terndingData, setTrendingData] = useState(null);
+  const [dailyNews, setDailyNews] = useState(null);
+  const [rates, setRates] = useState(null);
 
-  useEffect(()=>{
-    setIsMounted(true)
-    
-    if(isMounted){
-      checkLocation({setLocation, location, setWeather, toast})
-      getTrendingData({ setTrendingData, toast })
-      getDailyNews({ setDailyNews, toast })
+  useEffect(() => {
+    setIsMounted(true);
+
+    if (isMounted) {
+      checkLocation({ setLocation, location, setWeather, toast });
+      getTrendingData({ setTrendingData, toast });
+      getDailyNews({ setDailyNews, toast });
       // getNewestShorts({ setStatusData, toast })
-      getRates({c:currencyValue, setRates, toast, rates})
+      getRates({ c: currencyValue, setRates, toast, rates });
     }
-  }, [isMounted])
+  }, [isMounted]);
   // console.log('statusData', statusData)
 
-  useEffect(()=>{
-    setIsMounted(true)
-    document.title = `البحث عن: ${searchValue??''}`;
-    if(searchValue!=null){
+  useEffect(() => {
+    setIsMounted(true);
+    document.title = `البحث عن: ${searchValue ?? ""}`;
+    if (searchValue != null) {
       // console.log('searchValue', searchValue)
-      setIsLoading(true)
-      getfetchedData({searchValue, perPage, currentPage, setFechedData, setNewSearchData, setStatusData, setShortsTotal,
-        setTotalPages, setHasMore, setTotalAvailable, setSuggested, setIsLoading, toast, searchData, newSearchData})
+      setIsLoading(true);
+      getfetchedData({
+        searchValue,
+        perPage,
+        currentPage,
+        setFechedData,
+        setNewSearchData,
+        setStatusData,
+        setShortsTotal,
+        setTotalPages,
+        setHasMore,
+        setTotalAvailable,
+        setSuggested,
+        setIsLoading,
+        toast,
+        searchData,
+        newSearchData,
+      });
     }
-  }, [currentPage])
+  }, [currentPage]);
 
-  useEffect(()=>{
-    document.title = `البحث عن: ${searchValue??''}`;
-    setSearchCachValue(searchValue)
-    if(searchValue!=null && searchValue == initialSearchValue){
-      setIsLoading(true)
-      getfetchedData({searchValue, perPage, currentPage, setFechedData, setNewSearchData, setStatusData, setShortsTotal,
-        setTotalPages, setHasMore, setTotalAvailable, setSuggested, setIsLoading, toast, searchData, newSearchData})
-    }else if(searchValue != initialSearchValue){
-      const newLoc = `/search?s=${searchValue}&for=${searchTypeDropdownValue??''}`
-      history.replaceState(null, '',newLoc)
-      window.location.reload()
-      console.log('newLoc', newLoc)
+  useEffect(() => {
+    document.title = `البحث عن: ${searchValue ?? ""}`;
+    setSearchCachValue(searchValue);
+    if (searchValue != null && searchValue == initialSearchValue) {
+      setIsLoading(true);
+      getfetchedData({
+        searchValue,
+        perPage,
+        currentPage,
+        setFechedData,
+        setNewSearchData,
+        setStatusData,
+        setShortsTotal,
+        setTotalPages,
+        setHasMore,
+        setTotalAvailable,
+        setSuggested,
+        setIsLoading,
+        toast,
+        searchData,
+        newSearchData,
+      });
+    } else if (searchValue != initialSearchValue) {
+      const newLoc = `/search?s=${searchValue}&for=${
+        searchTypeDropdownValue ?? ""
+      }`;
+      history.replaceState(null, "", newLoc);
+      window.location.reload();
+      console.log("newLoc", newLoc);
     }
-  }, [searchValue])
+  }, [searchValue]);
 
-  useEffect(()=>{
-    console.log(searchCachValue != initialSearchValue, searchCachValue , initialSearchValue)
-    if(searchCachValue && searchCachValue != initialSearchValue){
-        const newLoc = `/search?s=${searchCachValue}&for=${searchTypeDropdownValue??''}`
-        router.replace(newLoc);
-      console.log('newLoc', newLoc);
-      }
-  }, [searchTypeDropdownValue])
-
-  useEffect(()=>{
-    if(isMounted){
-      getRates({c:currencyValue, setRates, toast})
+  useEffect(() => {
+    console.log(
+      searchCachValue != initialSearchValue,
+      searchCachValue,
+      initialSearchValue
+    );
+    if (searchCachValue && searchCachValue != initialSearchValue) {
+      const newLoc = `/search?s=${searchCachValue}&for=${
+        searchTypeDropdownValue ?? ""
+      }`;
+      router.replace(newLoc);
+      console.log("newLoc", newLoc);
     }
-  }, [currencyValue, isMounted])
+  }, [searchTypeDropdownValue]);
+
+  useEffect(() => {
+    if (isMounted) {
+      getRates({ c: currencyValue, setRates, toast });
+    }
+  }, [currencyValue, isMounted]);
 
   useEffect(() => {
     if (location && location.latitude && location.longitude) {
-      getTempData({ latitude: location.latitude, longitude: location.longitude, setWeather, toast });
+      getTempData({
+        latitude: location.latitude,
+        longitude: location.longitude,
+        setWeather,
+        toast,
+      });
     }
   }, [location]);
   // console.log('total', totalAvailable)
-  
+
   return (
-    <main className="w-full min-h-screen overflow-hidden relative bg-white dark:bg-black">
-      <SearchParamsComponent setSearchTypeDropdownValue={setSearchTypeDropdownValue} setSearchValue={setSearchValue}
-      setPerPage={setPerPage} perPage={perPage} setCurrentPage={setCurrentPage} setInitialSearchValue={setInitialSearchValue} />
+    <main className="w-full  min-h-screen overflow-hidden relative bg-white dark:bg-black">
+      <SearchParamsComponent
+        setSearchTypeDropdownValue={setSearchTypeDropdownValue}
+        setSearchValue={setSearchValue}
+        setPerPage={setPerPage}
+        perPage={perPage}
+        setCurrentPage={setCurrentPage}
+        setInitialSearchValue={setInitialSearchValue}
+      />
       <ToasterComponent />
       {/* blurred background background */}
       <div className="absolute inset-0 w-full h-[70vh] animate-hue-change">
-        <div className="size-full bg-cover bg-no-repeat animate-background-transition"
-        style={{
-          backgroundImage: `url(${bgImg?.src || bgImg})`,
-          filter: 'blur(80px)',
-        }} ></div>
+        <div
+          className="size-full bg-cover bg-no-repeat animate-background-transition"
+          style={{
+            backgroundImage: `url(${bgImg?.src || bgImg})`,
+            filter: "blur(80px)",
+          }}
+        ></div>
       </div>
       <div className="absolute inset-0 w-full h-1/2 bg-gradient-to-b from-white/5 to-white via-white via-[70vh] dark:via-black dark:from-black/5 dark:to-black" />
- 
-      <AppBar shadow='transparent' bgTransparent={false} searchValue={searchValue??''} setSearchValue={setSearchValue} activeTabIndex={activeTabIndex} 
-      setActiveTabIndex={setActiveTabIndex} searchCachValue={searchCachValue} setSearchCachValue={setSearchCachValue}
-      searchTypeDropdownValue={searchTypeDropdownValue} setSearchTypeDropdownValue={setSearchTypeDropdownValue} />
 
-      <SearchPage data={newSearchData} setBgImg={setBgImg} searchDropdownValue={searchTypeDropdownValue} setVidDis={setVidDis}
-      statusData={statusData} setCurrentVid={setCurrentVid} isLoading={isLoading} setCurrentProduct={setCurrentProduct} rates={rates}
-      weather={weather} setActiveVidIndex={setActiveVidIndex} setIsBlogPopupOpen={setIsBlogPopupOpen} setCurrentBlog={setCurrentBlog}
-      searchValue={searchValue??''} openStatus={setIsPopupOpen} activeTabIndex={activeTabIndex} setActiveTabIndex={setActiveTabIndex}
-      trendingData={terndingData} tagsData={fetchedData?.tags?.data} dailyNews={dailyNews} setIsProductPopupOpen={setIsProductPopupOpen}
-      currencyValue={currencyValue} setCurrencyValue={setCurrencyValue} totalPages={totalPages} setCurrentPage={setCurrentPage}
-      hasMore={hasMore} total={totalAvailable} currentPage={currentPage} suggested={suggested} shortsTotal={shortsTotal} /> 
+      <AppBar
+        shadow="transparent"
+        bgTransparent={false}
+        searchValue={searchValue ?? ""}
+        setSearchValue={setSearchValue}
+        activeTabIndex={activeTabIndex}
+        setActiveTabIndex={setActiveTabIndex}
+        searchCachValue={searchCachValue}
+        setSearchCachValue={setSearchCachValue}
+        searchTypeDropdownValue={searchTypeDropdownValue}
+        setSearchTypeDropdownValue={setSearchTypeDropdownValue}
+      />
 
-      <MediaPlayer isPopupOpen={isPopupOpen} setIsPopupOpen={setIsPopupOpen} isExpanded={isExpanded} vidDis={vidDis} 
-      setVidDis={setVidDis} setCurrentVid={setCurrentVid} currentVid={currentVid} setActiveVidIndex={setActiveVidIndex}
-      statusData={statusData} activeVidIndex={activeVidIndex}
-      setIsExpanded={setIsExpanded} actionDropdownValue={actionDropdownValue} setActionDropdownValue={setActionDropdownValue} />
+      <SearchPage
+        data={newSearchData}
+        setBgImg={setBgImg}
+        searchDropdownValue={searchTypeDropdownValue}
+        setVidDis={setVidDis}
+        statusData={statusData}
+        setCurrentVid={setCurrentVid}
+        isLoading={isLoading}
+        setCurrentProduct={setCurrentProduct}
+        rates={rates}
+        weather={weather}
+        setActiveVidIndex={setActiveVidIndex}
+        setIsBlogPopupOpen={setIsBlogPopupOpen}
+        setCurrentBlog={setCurrentBlog}
+        searchValue={searchValue ?? ""}
+        openStatus={setIsPopupOpen}
+        activeTabIndex={activeTabIndex}
+        setActiveTabIndex={setActiveTabIndex}
+        trendingData={terndingData}
+        tagsData={fetchedData?.tags?.data}
+        dailyNews={dailyNews}
+        setIsProductPopupOpen={setIsProductPopupOpen}
+        currencyValue={currencyValue}
+        setCurrencyValue={setCurrencyValue}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+        hasMore={hasMore}
+        total={totalAvailable}
+        currentPage={currentPage}
+        suggested={suggested}
+        shortsTotal={shortsTotal}
+      />
+
+      <Content />
+
+      <MediaPlayer
+        isPopupOpen={isPopupOpen}
+        setIsPopupOpen={setIsPopupOpen}
+        isExpanded={isExpanded}
+        vidDis={vidDis}
+        setVidDis={setVidDis}
+        setCurrentVid={setCurrentVid}
+        currentVid={currentVid}
+        setActiveVidIndex={setActiveVidIndex}
+        statusData={statusData}
+        activeVidIndex={activeVidIndex}
+        setIsExpanded={setIsExpanded}
+        actionDropdownValue={actionDropdownValue}
+        setActionDropdownValue={setActionDropdownValue}
+      />
 
       {/* <BlogsPlayer isPopupOpen={isBlogPopupOpen} setIsPopupOpen={setIsBlogPopupOpen} setCurrentBlog={setCurrentBlog} currentBlog={currentBlog} /> */}
-      <ProductsPlayer isPopupOpen={isProductPopupOpen} setIsPopupOpen={setIsProductPopupOpen} setCurrentProduct={setCurrentProduct} currentProduct={currentProduct} />
+      <ProductsPlayer
+        isPopupOpen={isProductPopupOpen}
+        setIsPopupOpen={setIsProductPopupOpen}
+        setCurrentProduct={setCurrentProduct}
+        currentProduct={currentProduct}
+      />
       <Footer />
     </main>
   );
