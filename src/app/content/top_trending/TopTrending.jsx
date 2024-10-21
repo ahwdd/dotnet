eccccd5b3dc97193595cbbb0de3363d73b786877
ogamal-user <ogamal@arabhardware.net>
2024-10-21 13:25:09 +0300

    [MODIFY] top trending section

--- src/app/content/top_trending/TopTrending.jsx
+++ src/app/content/top_trending/TopTrending.jsx
@@ -13,9 +13,22 @@ function TopTrending() {
     <div className="top-trending">
       <div className="top-trending-wrapper w-full bg-red-500 h-[500px] flex justify-center">
         <div className="top-trending-container bg-green-500 overflow-hidden w-1/2 border-red-500 border-2 border-solid">
-          <div className="top-trending-slider">
+          <div className="top-trending-slider !flex gap-1">
             {/* Original Content */}
-            <div className="top-trending-content bg-blue-500 w-[300px] hidden">
+            <div className="top-trending-content w-full !flex justify-between">
+              <p>مستجدات الحرب علي غزة</p>
+              <p>مستجدات الحرب علي غزة</p>
+              <p>مستجدات الحرب علي غزة</p>
+              <p>مستجدات الحرب علي غزة</p>
+            </div>
+            <div className="top-trending-content w-full !flex justify-between -mr-[100% ]">
+              <p>مستجدات الحرب علي غزة</p>
+              <p>مستجدات الحرب علي غزة</p>
+              <p>مستجدات الحرب علي غزة</p>
+              <p>مستجدات الحرب علي غزة</p>
+            </div>
+
+            <div className="top-trending-content w-full !flex justify-between -mr-[100% ]">
               <p>مستجدات الحرب علي غزة</p>
               <p>مستجدات الحرب علي غزة</p>
               <p>مستجدات الحرب علي غزة</p>
